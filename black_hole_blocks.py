e342c61aa2c202ec450056102b3500d8f5de6f0d
Brian Pollack <brian.pollack@cern.ch>
2015-11-13 10:36:36 -0600

    cleaning house

--- black_hole_blocks.py
+++ black_hole_blocks.py
@@ -14,45 +14,74 @@ plt.close('all')
 normed = True
 log = True
 try:
-    ST_dict = pkl.load(open( "files/BHTree.p", "rb" ))
+    ST_dict_data = pkl.load(open( "files/BHTree.p", "rb" ))
     print 'pickle loaded'
 except:
     plt.close('all')
     infile = TFile('files/BHTree.root')
     intree = infile.Get('BH_Tree')
     entries = intree.GetEntriesFast()
-    ST_dict = {}
+    ST_dict_data = {}
     for i in range(2,11):
-        ST_dict['ST_mul{}'.format(i)] = []
+        ST_dict_data['ST_mul{}'.format(i)] = []
 
     for i in xrange(entries):
         if i%10000==0: print i
         ientry = intree.LoadTree(i)
         branch = intree.GetEntry(ientry)
-        ST_dict['ST_mul2'].append(intree.ST_mul2)
-        ST_dict['ST_mul3'].append(intree.ST_mul3)
-        ST_dict['ST_mul4'].append(intree.ST_mul4)
-        ST_dict['ST_mul5'].append(intree.ST_mul5)
-        ST_dict['ST_mul6'].append(intree.ST_mul6)
-        ST_dict['ST_mul7'].append(intree.ST_mul7)
-        ST_dict['ST_mul8'].append(intree.ST_mul8)
-        ST_dict['ST_mul9'].append(intree.ST_mul9)
-        ST_dict['ST_mul10'].append(intree.ST_mul10)
+        ST_dict_data['ST_mul2'].append(intree.ST_mul2)
+        ST_dict_data['ST_mul3'].append(intree.ST_mul3)
+        ST_dict_data['ST_mul4'].append(intree.ST_mul4)
+        ST_dict_data['ST_mul5'].append(intree.ST_mul5)
+        ST_dict_data['ST_mul6'].append(intree.ST_mul6)
+        ST_dict_data['ST_mul7'].append(intree.ST_mul7)
+        ST_dict_data['ST_mul8'].append(intree.ST_mul8)
+        ST_dict_data['ST_mul9'].append(intree.ST_mul9)
+        ST_dict_data['ST_mul10'].append(intree.ST_mul10)
     infile.Close()
-    pkl.dump(ST_dict, open( "files/BHTree.p", "wb" ), protocol = -1)
+    pkl.dump(ST_dict_data, open( "files/BHTree.p", "wb" ), protocol = -1)
+
+try:
+    ST_dict_MC = pkl.load(open( "files/BHTree_QCD2000.p", "rb" ))
+    print 'pickle loaded'
+except:
+    plt.close('all')
+    infile = TFile('files/BH_Tree_QCD_HT-2000_inf_25ns.root')
+    intree = infile.Get('BH_Tree')
+    entries = intree.GetEntriesFast()
+    ST_dict_MC = {}
+    for i in range(2,11):
+        ST_dict_MC['ST_mul{}'.format(i)] = []
+
+    for i in xrange(entries):
+        if i%10000==0: print i
+        ientry = intree.LoadTree(i)
+        branch = intree.GetEntry(ientry)
+        ST_dict_MC['ST_mul2'].append(intree.ST_mul2)
+        ST_dict_MC['ST_mul3'].append(intree.ST_mul3)
+        ST_dict_MC['ST_mul4'].append(intree.ST_mul4)
+        ST_dict_MC['ST_mul5'].append(intree.ST_mul5)
+        ST_dict_MC['ST_mul6'].append(intree.ST_mul6)
+        ST_dict_MC['ST_mul7'].append(intree.ST_mul7)
+        ST_dict_MC['ST_mul8'].append(intree.ST_mul8)
+        ST_dict_MC['ST_mul9'].append(intree.ST_mul9)
+        ST_dict_MC['ST_mul10'].append(intree.ST_mul10)
+    infile.Close()
+    pkl.dump(ST_dict_MC, open( "files/BHTree_QCD2000.p", "wb" ), protocol = -1)
 
 
-for key in ST_dict.iterkeys():
-    if key != 'ST_mul5': continue
-    ST_dict[key]=[i for i in  ST_dict[key] if i>1900]
-    print len(ST_dict[key])
+for key in ST_dict_data.iterkeys():
+    #if key != 'ST_mul10': continue
+    print len(ST_dict_data[key])
+    ST_dict_data[key]=[i for i in  ST_dict_data[key] if i>1900]
+    print key, len(ST_dict_data[key])
+    a = ST_dict_data[key]
     fig = plt.figure()
     ax = plt.gca()
-    hist(ST_dict[key], bins=200, histtype='bar',alpha=0.2, label='standard histogram',normed=normed,log=log)
+    hist(ST_dict_data[key], bins=200, histtype='bar',alpha=0.2, label='standard histogram',normed=normed,log=log)
     print 'doing bb'
-    p0 = int(re.sub("\D",'',key))
-    p0=0.05
-    hist(ST_dict[key], 'blocks', fitness = 'events', p0=p0,  ax = ax, histtype='step', label='Bayesian Blocks', linewidth=2,normed=normed,log=log)
+    p0=0.01
+    hist(ST_dict_data[key], 'blocks', fitness = 'events', p0=p0,  ax = ax, histtype='step', label='Bayesian Blocks', linewidth=2,normed=normed,log=log)
     ax.legend()
     plt.title(key)
     plt.xlabel('ST (GeV)')
@@ -63,6 +92,25 @@ for key in ST_dict.iterkeys():
     plt.savefig('plots/'+key+'.pdf')
     plt.show()
 
-
-
-
+'''
+for key in ST_dict_MC.iterkeys():
+    #if key != 'ST_mul2': continue
+    ST_dict_MC[key]=[i for i in  ST_dict_MC[key] if i>1900][:15000]
+    print len(ST_dict_MC[key])
+    fig = plt.figure()
+    ax = plt.gca()
+    hist(ST_dict_MC[key], bins=200, histtype='bar',alpha=0.2, label='standard histogram',normed=normed,log=log)
+    print 'doing bb'
+    p0 = int(re.sub("\D",'',key))
+    p0=0.05
+    hist(ST_dict_MC[key], 'blocks', fitness = 'events', p0=p0,  ax = ax, histtype='step', label='Bayesian Blocks', linewidth=2,normed=normed,log=log)
+    ax.legend()
+    plt.title(''.join([key,'QCD HT2000']))
+    plt.xlabel('ST (GeV)')
+    if normed:
+        plt.ylabel(r'N/$\Sigma$N$\Delta$x')
+    else:
+        plt.ylabel('N')
+    plt.savefig('plots/'+key+'_MC_.pdf')
+    plt.show()
+'''
--- black_hole_test.py
+++ black_hole_test.py
@@ -5,7 +5,10 @@ import numpy as np
 from scipy import stats
 from matplotlib import pyplot as plt
 from astroML.plotting import hist
+from bayesian_blocks_modified import bayesian_blocks
 import scipy.stats as st
+from fill_between_steps import fill_between_steps
+import cPickle as pkl
 
 import ROOT
 from ROOT import TFile
@@ -16,26 +19,59 @@ from ROOT import TRandom3
 plt.close('all')
 normed = True
 log = True
-
+mult = 'ST_mul5'
+#mult = 'ST_mul2'
 ROOT.gRandom.SetSeed(8675309)
+
+ST_dict_data = pkl.load(open( "files/BHTree.p", "rb" ))
+print 'pickle loaded'
+
+ST_dict_data[mult]=[i for i in  ST_dict_data[mult] if i>1900]
+nentries = len(ST_dict_data[mult])
+print nentries
+
 my_pdf = TF1("my_pdf","2.3949e6*3.74/(0.67472+x)**10.1809",1900,5500)
+#my_pdf = TF1("my_pdf","2.3949e6/(0.67472+x)**10.1809",1900,5500)
 my_rands = []
-for i in xrange(14653):
+#for i in xrange(nentries):
+for i in xrange(50000):
     my_rands.append(my_pdf.GetRandom())
-    if i == 0: print my_rands[-1]
+
+normed_counts_mc, bb_edges = np.histogram(my_rands,bayesian_blocks(my_rands), density=True)
+normed_counts_data, _= np.histogram(ST_dict_data[mult],bb_edges, density=True)
+counts_mc, _= np.histogram(my_rands,bb_edges)
+counts_data, _= np.histogram(ST_dict_data[mult],bb_edges)
+
+rescaled_counts_mc = normed_counts_mc*nentries
+rescaled_counts_data = normed_counts_data*nentries
+bin_centres = (bb_edges[:-1] + bb_edges[1:])/2.
+
+rescaled_err = np.sqrt(counts_data)/(bb_edges[1:]-bb_edges[:-1])
+err = np.sqrt(counts_data)
 
 fig = plt.figure()
 ax = plt.gca()
-hist(my_rands, bins=200, histtype='bar',alpha=0.2, label='standard histogram', log=log, normed=normed)
-hist(my_rands, bins='blocks', histtype='step',linewidth=2, label='bb',log=log, normed=normed)
-plt.legend()
-plt.title('MC gen from ST_mul5 fit function')
+ax.set_yscale("log", nonposy='clip')
+fill_between_steps(ax, bb_edges, rescaled_counts_mc,1e-3, alpha=0.2, step_where='pre',linewidth=0,label='fit MC')
+ax.errorbar(bin_centres, rescaled_counts_data, yerr=rescaled_err, fmt='ok',label='data')
+#plt.semilogy()
+ax.legend()
+plt.title('MC gen from '+mult+' fit function and real data')
 plt.xlabel('ST (GeV)')
-if normed:
-    plt.ylabel(r'N/$\Sigma$N$\Delta$x')
-else:
-    plt.ylabel('N')
+plt.ylabel(r'N/$\Delta$x')
 plt.show()
-plt.savefig('plots/ST_mul5_fit.pdf')
+plt.savefig('plots/'+mult+'_fit_and_data_normed.pdf')
 
+fig = plt.figure()
+ax = plt.gca()
+ax.set_yscale("log", nonposy='clip')
+fill_between_steps(ax, bb_edges, counts_mc,1e-3, alpha=0.2, step_where='pre',linewidth=0,label='fit MC')
+ax.errorbar(bin_centres, counts_data, yerr=err, fmt='ok',label='data')
+#plt.semilogy()
+ax.legend()
+plt.title('MC gen from '+mult+' fit function and real data')
+plt.xlabel('ST (GeV)')
+plt.ylabel('N')
+plt.show()
+plt.savefig('plots/'+mult+'_fit_and_data.pdf')
 
