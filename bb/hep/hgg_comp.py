367ab657c19d698e365b152662586af7c4e5a6c3
Brian Pollack <brian.pollack@cern.ch>
2016-06-06 16:53:03 -0500

    hgg_comp updated with lots of functions, less mess

--- bb/hep/hgg_comp.py
+++ bb/hep/hgg_comp.py
@@ -7,10 +7,10 @@ import bisect
 import numpy as np
 from scipy.stats import norm
 import scipy.integrate as integrate
+from scipy.stats import poisson
 import cPickle as pkl
 from matplotlib import pyplot as plt
-# from bb.tools.bayesian_blocks_modified import bayesian_blocks
-from bb.tools.bb_plotter import make_fit_plot, make_bb_plot
+from bb.tools.bayesian_blocks_modified import bayesian_blocks
 import nllfitter.future_fitter as ff
 from ROOT import gRandom
 from ROOT import TF1
@@ -190,6 +190,7 @@ def get_mismatch_metric_v3(bc_nominal, be_nominal, bc_test, be_test, main_edge):
 
 
 def calc_local_pvalue(N_bg, var_bg, N_sig, var_sig, ntoys=1e7):
+    '''Not an accurate estimate for large sigma'''
     print ''
     print 'Calculating local p-value and significance based on {0} toys'.format(ntoys)
     print 'N_bg = {0}, sigma_bg = {1}, N_signal = {2}'.format(N_bg,var_bg,N_sig)
@@ -200,33 +201,18 @@ def calc_local_pvalue(N_bg, var_bg, N_sig, var_sig, ntoys=1e7):
     print 'local p-value = {0}'.format(pval)
     print 'local significance = {0:.2f}'.format(np.abs(norm.ppf(1-pval)))
 
-
-if __name__ == "__main__":
-    plt.close('all')
-    current_dir = os.path.dirname(__file__)
-    bb_dir      = os.path.join(current_dir, '../..')
-    hgg_bg      = pkl.load(open(bb_dir+'/files/hgg_bg.p', "rb"))
-    hgg_signal  = pkl.load(open(bb_dir+'/files/hgg_signal.p', "rb"))
-
-# grab a handful of bg events, and an ~X sigma number of signal events
-    n_sigma              = 5
+def generate_initial_params(hgg_bg, hgg_signal, n_sigma):
+    '''Input bg and signal dataframes, and a sigma value for signal injection.
+    Output parameters for the pdfs that describe those distributions.'''
+    # grab a handful of bg events, and an ~X sigma number of signal events
     hgg_bg_selection     = hgg_bg[(hgg_bg.Mgg > 100) & (hgg_bg.Mgg < 180)][0:10000].Mgg
     n_bg_under_sig       = hgg_bg_selection[(118 < hgg_bg_selection) & (hgg_bg_selection < 133)].size
     n_sig                = int(n_sigma*np.sqrt(n_bg_under_sig))
     hgg_signal_selection = hgg_signal[(hgg_signal.Mgg >= 118) & (hgg_signal.Mgg <= 133)][0:n_sig].Mgg
     data_bg              = hgg_bg_selection.values
     data_sig             = hgg_signal_selection.values
-    data_bg_sig          = np.concatenate((data_bg, data_sig))
 
-    print 'loaded'
-
-# setup initial ranges and binning
-    xlimits = (100., 180.)
-    x       = np.linspace(xlimits[0], xlimits[1], 10000)
-    nbins   = 80
-    binning = (xlimits[1]-xlimits[0])/nbins
-
-# fit to the data distributions
+    # fit to the data distributions
     bg_model = ff.Model(bg_pdf, ['a1', 'a2', 'a3'])
     bg_model.set_bounds([(-1., 1.), (-1., 1.), (-1., 1.)])
     bg_fitter = ff.NLLFitter(bg_model, data_bg)
@@ -237,86 +223,92 @@ if __name__ == "__main__":
     sig_fitter = ff.NLLFitter(sig_model, data_sig)
     sig_result = sig_fitter.fit([120.0, 2])
 
-    bg_sig_model = ff.Model(bg_sig_pdf, ['C', 'mu', 'sigma', 'a1', 'a2', 'a3'])
-    bg_sig_model.set_bounds([(0, 1), (xlimits[0], xlimits[1]), (0, 50), (-1., 1.), (-1., 1.), (-1., 1.)])
-    bg_sig_fitter = ff.NLLFitter(bg_sig_model, data_bg_sig)
-    bg_sig_result = bg_sig_fitter.fit([0.01, 125.0, 2, 0.0, 0.0, 0.0])
+    n_bg = len(data_bg)
 
-# make MC
-    gRandom.SetSeed(111)
-# bg dist
+    be_bg = bayesian_blocks(data_bg, p0=0.02)
+    be_sig = bayesian_blocks(data_sig, p0=0.02)
+
+    return bg_result, sig_result, n_bg, n_sig, be_bg, be_sig
+
+def generate_toy_data(bg_params, sig_params, n_bg, n_sig, seed=None):
+    '''use bg and signal params to generated simulated data'''
+    if seed:
+        gRandom.SetSeed(seed)
+
+    # bg dist
     bg_pdf_ROOT = functools.partial(bg_pdf, doROOT=True)
     tf1_bg_pdf = TF1("tf1_bg_pdf", bg_pdf_ROOT, 100, 180, 3)
-    tf1_bg_pdf.SetParameters(*bg_result.x)
-# signal dist
+    tf1_bg_pdf.SetParameters(*bg_params)
+    # signal dist
     sig_pdf_ROOT = functools.partial(sig_pdf, doROOT=True)
     tf1_sig_pdf = TF1("tf1_sig_pdf", sig_pdf_ROOT, 100, 180, 2)
-    tf1_sig_pdf.SetParameters(*sig_result.x)
-    mc_bg = [tf1_bg_pdf.GetRandom() for i in xrange(len(data_bg))]
-    mc_sig = [tf1_sig_pdf.GetRandom() for i in xrange(len(data_sig))]
+    tf1_sig_pdf.SetParameters(*sig_params)
+    mc_bg = [tf1_bg_pdf.GetRandom() for i in xrange(n_bg)]
+    mc_sig = [tf1_sig_pdf.GetRandom() for i in xrange(n_sig)]
     mc_bg_sig = mc_bg+mc_sig
-    mc_bg_sig_fitter = ff.NLLFitter(bg_sig_model, np.asarray(mc_bg_sig))
-    mc_bg_sig_result = mc_bg_sig_fitter.fit([0.01, 125.0, 2, 0.0, 0.0, 0.0])
-    mc_bg_bad_fitter = ff.NLLFitter(bg_model, np.asarray(mc_bg_sig))
-    mc_bg_bad_result = mc_bg_bad_fitter.fit([ 0.0, 0.0, 0.0])
+    return mc_bg_sig
 
-    n_bg_calc, _  = integrate.quad(functools.partial(bg_sig_pdf, a=np.concatenate([[0], mc_bg_sig_result.x[1:]])),
-            mc_bg_sig_result.x[1]-mc_bg_sig_result.x[2]*2, mc_bg_sig_result.x[1]+mc_bg_sig_result.x[2]*2)
-    n_bg_calc *= len(mc_bg_sig)*(1-mc_bg_sig_result.x[0])
-    mc_bg_sig_result.x[1]-mc_bg_sig_result.x[2]*2, mc_bg_sig_result.x[1]+mc_bg_sig_result.x[2]*2
-    n_sig_calc = len(mc_bg_sig)*mc_bg_sig_result.x[0]
-    n_sig_std = mc_bg_sig_fitter.get_corr(mc_bg_sig_result.x)[0][0]
-    print n_sig_calc, n_sig_std*len(mc_bg_sig), n_bg_calc, np.sqrt(n_bg_calc)
-    calc_local_pvalue(n_bg_calc, np.sqrt(n_bg_calc), n_sig, n_sig_std*len(mc_bg_sig),1e7)
+def generate_q0_via_fits(data, bg_params=None, sig_params=None):
+    '''Perform two nll fits to data, one for bg+signal, one for bg-only.
+    Use these values to create the q0 statistic.'''
+
+    bg_model = ff.Model(bg_pdf, ['a1', 'a2', 'a3'])
+    bg_model.set_bounds([(-1., 1.), (-1., 1.), (-1., 1.)])
+
+    bg_sig_model = ff.Model(bg_sig_pdf, ['C', 'mu', 'sigma', 'a1', 'a2', 'a3'])
+    bg_sig_model.set_bounds([(0, 1), (115, 135), (0.4, 9), (-1., 1.), (-1., 1.), (-1., 1.)])
+
+    mc_bg_sig_fitter = ff.NLLFitter(bg_sig_model, np.asarray(data),verbose=False)
+    mc_bg_sig_result = mc_bg_sig_fitter.fit([0.01, 125.0, 2, 0.0, 0.0, 0.0], calculate_corr = False)
+    mc_bg_only_fitter = ff.NLLFitter(bg_model, np.asarray(data),verbose=False)
+    mc_bg_only_result = mc_bg_only_fitter.fit([ 0.0, 0.0, 0.0], calculate_corr = False)
+    q0 = 2*max(mc_bg_only_result.fun-mc_bg_sig_result.fun,0)
+    return q0
+
+def generate_q0_via_bins(data, be_hybrid, true_bg_bc, true_sig_bc):
+    '''Generate likelihood ratios based on poisson distributions for each bin
+    in binned data.  True values for bg and bg+signal are determined from integration of
+    underlying pdfs used to generate toys.
+    Use these values to create the q0 statistic.'''
+
+    bc_hybrid, _ = np.histogram(data, be_hybrid)
+    l_bg  = 1
+    l_sig = 1
+    for i in range(len(be_hybrid)-1):
+        l_bg  *= poisson.pmf(bc_hybrid[i], true_bg_bc[i])
+        l_sig *= poisson.pmf(bc_hybrid[i], true_bg_bc[i]+true_sig_bc[i])
+
+    q0 = -2*(np.log(l_bg)-np.log(l_sig))
+    return q0
+
+if __name__ == "__main__":
+    plt.close('all')
+    current_dir = os.path.dirname(__file__)
+    bb_dir      = os.path.join(current_dir, '../..')
+    hgg_bg      = pkl.load(open(bb_dir+'/files/hgg_bg.p', "rb"))
+    hgg_signal  = pkl.load(open(bb_dir+'/files/hgg_signal.p', "rb"))
+
+    bg_result, sig_result, n_bg, n_sig, be_bg, be_sig = generate_initial_params(hgg_bg, hgg_signal, 5)
+    be_hybrid = np.concatenate([be_bg[be_bg<be_sig[0]-1.5], be_sig, be_bg[be_bg>be_sig[-1]+1.5]])
+
+    true_bg_bc  = []
+    true_sig_bc = []
+    for i in range(len(be_hybrid)-1):
+	true_bg, _   = integrate.quad(functools.partial(bg_pdf, a=bg_result.x),be_hybrid[i],be_hybrid[i+1])
+	true_bg_bc.append(true_bg*n_bg)
+	true_sig, _  = integrate.quad(functools.partial(sig_pdf, a=sig_result.x),be_hybrid[i],be_hybrid[i+1])
+	true_sig_bc.append(true_sig*n_sig)
+
+    signif_fit_hist = []
+    signif_bb_hist = []
+
+    # Do a bunch of toys
+    for i in range(500):
+        if i%10==0: print 'doing fit #',i
+        mc_bg_sig = generate_toy_data(bg_result.x, sig_result.x, n_bg, n_sig)
+        q0_fit = generate_q0_via_fits(mc_bg_sig)
+        signif_fit_hist.append(np.sqrt(q0_fit))
+        q0_bb = generate_q0_via_bins(mc_bg_sig, be_hybrid, true_bg_bc, true_sig_bc)
+        signif_bb_hist.append(np.sqrt(q0_bb))
 
 
-# plot some outputs
-
-#make_fit_plot(data_bg, 80, xlimits, functools.partial(bg_pdf, a=bg_result.x),
-#        'Background distribution')
-#make_fit_plot(data_sig, 80, xlimits, functools.partial(sig_pdf, a=sig_result.x),
-#        'Signal distribution')
-#make_fit_plot(data_bg_sig, 80, xlimits, functools.partial(bg_sig_pdf, a=bg_sig_result.x),
-#        'Signal+Background distribution', extra_pdf_tuple=(functools.partial(bg_sig_pdf,
-#            a=np.concatenate([[0], bg_sig_result.x[1:]])), 1-bg_sig_result.x[0], 'bg pdf'))
-    textstr = ('$C={0:.2f}$\n$\mu={1:.2f}$\n$\sigma={2:.2f}$\n'
-                '$a_1={3:.2f}$\n$a_2={4:.2f}$\n$a_3={5:.2f}$').format(*mc_bg_sig_result.x)
-    make_fit_plot(mc_bg_sig, 80, xlimits, functools.partial(bg_sig_pdf, a=mc_bg_sig_result.x),
-            'Signal+Background MC Toy', extra_pdf_tuple=(functools.partial(bg_sig_pdf,
-                a=np.concatenate([[0], mc_bg_sig_result.x[1:]])), 1-mc_bg_sig_result.x[0], 'bg pdf'), textstr = textstr)
-#make_bb_plot(data_bg_sig, 0.02, bb_dir+'/plots/', range=xlimits, title=r'pp$\to\gamma\gamma$ Sim',
-#             xlabel=r'$m_{\gamma\gamma}$ (GeV)', ylabel='P/bin', save_name='hgg_inject_hist')
-#make_bb_plot(data_bg, 0.02, bb_dir+'/plots/', range=xlimits, title=r'pp$\to\gamma\gamma$ Sim',
-#             xlabel=r'$m_{\gamma\gamma}$ (GeV)', ylabel='P/bin', save_name='hgg_bg_hist')
-#make_bb_plot(data_sig, 0.02, bb_dir+'/plots/', range=xlimits, title=r'pp$\to\gamma\gamma$ Sim',
-#        xlabel=r'$m_{\gamma\gamma}$ (GeV)', ylabel='P/bin', save_name='hgg_sig_hist')
-    bc_bg, be_bg = make_bb_plot(mc_bg, 0.02, bb_dir+'/plots/', range=xlimits, title='BB binning for Background Distribution',
-                   xlabel=r'$m_{\gamma\gamma}$ (GeV)', ylabel='P/bin', save_name='hgg_bg_hist')
-    bc_sig, be_sig = make_bb_plot(mc_sig, 0.02, bb_dir+'/plots/', range=xlimits, title='BB binning for Signal Distribution',
-                     xlabel=r'$m_{\gamma\gamma}$ (GeV)', ylabel='P/bin', save_name='hgg_sig_hist')
-
-
-    plt.show()
-
-# convert be_sig to widths:
-
-
-# run bb on many MC toys
-
-#bg_bc = {}
-#bg_be = {}
-#bg_sig_bc = {}
-#bg_sig_be = {}
-#for toy in range(100):
-#    print 'toy', toy
-#    mc_bg                                = [tf1_bg_pdf.GetRandom() for i in xrange(len(data_bg))]
-#    mc_sig                               = [tf1_sig_pdf.GetRandom() for i in xrange(len(data_sig))]
-#    mc_bg_sig                            = mc_bg+mc_sig
-#    bg_bin_content, bg_bin_edges         = np.histogram(mc_bg, bayesian_blocks(mc_bg, p0=0.02), density=True)
-#    bg_sig_bin_content, bg_sig_bin_edges = np.histogram(mc_bg_sig, bayesian_blocks(mc_bg_sig, p0=0.02), density=True)
-#    bg_bc['toy'+str(toy)]                = bg_bin_content
-#    bg_be['toy'+str(toy)]                = bg_bin_edges
-#    bg_sig_bc['toy'+str(toy)]            = bg_sig_bin_content
-#    bg_sig_be['toy'+str(toy)]            = bg_sig_bin_edges
-
-# do stuff with these bcs and bes
